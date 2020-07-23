bf04c1a3f3098e92fb298db7dd482bcd3b9b5a7e
happyhar <65075165+happyhar@users.noreply.github.com>
2020-07-23 13:34:30 +0530

    Update Timer.py

--- Timer.py
+++ Timer.py
@@ -1,15 +1,8 @@
 import time
-timing = input("Enter the timings")
-run = raw_input("Start? > ")
-mins = 0
-# Only run if the user types in "start"
 
-if run == "start":
-    # Loop until we reach  minutes running
-    while mins != int(timing):
-        print("You were Deceived. Your Time Is Over At {}".format(mins))
-        # Sleep for a minute
-        time.sleep(60)
-        # Increment the minute total
-        mins += 1
-    # Bring up the dialog box here
+# Takes input of timings
+t = int(input("Enter The Timings in minutes: "))
+t=t*60
+time.sleep(t)
+# Prints the message that time is over
+print("You are Deceived")
